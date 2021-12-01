af71811fe873f6e348d01327e89c6a945718cb01
Thryrallo <thryrallo@gmail.com>
2021-12-01 22:14:54 +0100

    Private room setup instructions

---
+++ PrivateRoom/PrivateRoomMain.cs
@@ -0,0 +1,21 @@
+﻿
+using Thry.General;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Thry.Udon.PrivateRoom
+{
+    public class PrivateRoomMain : UdonSharpBehaviour
+    {
+        public ThryAction doorOutside;
+        public ThryAction doorInside;
+
+        private void Start()
+        {
+            doorOutside.teleportTarget = doorInside.transform;
+            doorInside.teleportTarget = doorOutside.transform;
+        }
+    }
+}
