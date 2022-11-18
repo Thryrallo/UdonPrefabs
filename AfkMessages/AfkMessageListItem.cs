c34adfb688b014ab43a09d5370b48eadf41b81aa
Thryrallo <thryrallo@gmail.com>
2022-11-18 23:24:23 +0100

    Added AfkMessages

---
+++ AfkMessages/AfkMessageListItem.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AfkMessageListItem : UdonSharpBehaviour
+{
+    public Text User;
+    public Text Message;
+    public Text Time;
+
+    public void Set(string user, string message, string time)
+    {
+        User.text = user;
+        Message.text = message;
+        Time.text = time;
+    }
+}
